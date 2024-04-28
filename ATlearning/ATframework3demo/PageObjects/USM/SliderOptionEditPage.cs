bf9dcb9fc3663312a5e8b088d886c65dfe9590db
Davidlinkin <163144804+Davidlinkin@users.noreply.github.com>
2024-04-28 13:48:39 +0200

    Добавил тест "Редактирование заголовка у опции"
    Создал Page Objects для работы с формой просмотра и редактирования опции. Прописал логику теста.

--- ATlearning/ATframework3demo/PageObjects/USM/SliderOptionEditPage.cs
+++ ATlearning/ATframework3demo/PageObjects/USM/SliderOptionEditPage.cs
@@ -1,7 +1,30 @@
-﻿namespace ATframework3demo.PageObjects.USM
+﻿using atFrameWork2.SeleniumFramework;
+using ATframework3demo.TestEntities;
+using OpenQA.Selenium;
+
+namespace ATframework3demo.PageObjects.USM
 {
     public class SliderOptionEditPage
     {
+        WebItem inputOptionEditForm =>
+            new WebItem("//input[@data-bx-id='task-edit-title']",
+                "Поле ввода заголовка опции");
+        WebItem btnSaveChange =>
+            new WebItem("//button[@data-bx-id='task-edit-submit']",
+                "Кнопка 'сохранить изменения'");
+        public SliderOptionEditPage SetNameInEditForm(Bitrix24USMOption newTestTitle)
+        {
+            inputOptionEditForm.SendKeys(Keys.Control + "a");
+            inputOptionEditForm.SendKeys(Keys.Delete);
+            inputOptionEditForm.SendKeys(newTestTitle.Title);
+            return this;
+        }
+
+        public SliderOptionViewPage SaveChangesInOption()
+        {
+            btnSaveChange.Click();
+            return new SliderOptionViewPage();
+        }
 
     }
 }
---
+++ ATlearning/ATframework3demo/TestCases/Case_Bitrix24_USM_Editing_Оption_Name.cs
@@ -0,0 +1,62 @@
+﻿using atFrameWork2.BaseFramework;
+using atFrameWork2.PageObjects;
+using atFrameWork2.TestEntities;
+using ATframework3demo.TestEntities;
+using atFrameWork2.BaseFramework.LogTools;
+
+namespace ATframework3demo.TestCases
+{
+    public class Case_Bitrix24_USM_Editing_Оption_Name : CaseCollectionBuilder
+    {
+        protected override List<TestCase> GetCases()
+        {
+            var caseCollection = new List<TestCase>();
+            caseCollection.Add(new TestCase("Редактирование названиея опции в USM", homePage => EditingOptionNameUSM(homePage)));
+            return caseCollection;
+        }
+        void EditingOptionNameUSM(PortalHomePage homePage)
+        {
+            string scrumTeamName = "Новая скрам-команда " + DateTime.Now.Ticks;
+            string scrumMasterName = "David Zhemaitis";
+            var testScrumDetails = new Bitrix24ScrumTeamDetail(scrumTeamName, scrumMasterName);
+            string optionTitle = "testTitle " + DateTime.Now.Ticks;
+            var testTitle = new Bitrix24USMOption(optionTitle);
+            string newOptionTitle = "editedTitle " + DateTime.Now.Ticks;
+            var newTestTitle = new Bitrix24USMOption(newOptionTitle);
+            var usmPageAfterCreation = homePage
+                .LeftMenu
+                .OpenTasks()
+                .OpenScram()
+                .CreateScrumTeam()
+                .FillScrumTeamForm(testScrumDetails)
+                .CreateUSM()
+                .CreateOption(testTitle);
+            bool isOptionCreated = usmPageAfterCreation.IsOptionExist(testTitle);
+            if (!isOptionCreated)
+            {
+                Log.Error($"<b>Опция с исходным заголовком '{testTitle.Title}' не найдена</b>");
+                return;
+            }
+            else
+            {
+                Log.Info($"<b>Опция с исходным заголовком '{testTitle.Title}' найдена</b>");
+            }
+            var usmPageWithOption = usmPageAfterCreation
+                .OpenOptionView(testTitle)
+                .OpenOptionEditForm()
+                .SetNameInEditForm(newTestTitle)
+                .SaveChangesInOption()
+                .CloseOptionViewForm()
+                .RefreshPage(); 
+            bool isOptionPresent = usmPageWithOption.IsOptionExist(newTestTitle);
+            if (!isOptionPresent)
+            {
+                Log.Error($"<b>Опция с новым заголовком '{newTestTitle.Title}' не найдена</b>");
+            }
+            else
+            {
+                Log.Info($"<b>Опция с новым заголовком '{newTestTitle.Title}' найдена</b>");
+            }
+        }
+    }
+}
