9d574f8823cd2d178f5087661176e3f0c9198597
Davidlinkin <163144804+Davidlinkin@users.noreply.github.com>
2024-04-29 22:15:53 +0200

    Исправление: изменил наименование аргумента типа Bitrix24USMOption
    Изменил наименование аргумента типа Bitrix24USMOption, а также описание переменной inputNewOption

--- ATlearning/ATframework3demo/PageObjects/USM/UserStoryMapPage.cs
+++ ATlearning/ATframework3demo/PageObjects/USM/UserStoryMapPage.cs
@@ -14,35 +14,35 @@ namespace ATframework3demo.PageObjects.USM
                 "Кнопка добавления опции на USM");
         WebItem inputNewOption =>
             new WebItem("//div[@class='dndrop-container vertical']//input[@type='text']",
-                "Созданная пустая опция");
-        private WebItem GetOptionLinkByTitle(Bitrix24USMOption testTitle)
+                "Поле для заполнения названия созданной пустой опции на USM");
+        private WebItem GetOptionLinkByTitle(Bitrix24USMOption optionDetail)
         {
-            return new WebItem($"//div[@class='option']//a[contains(text(), '{testTitle.Title}')]",
-                $"Ссылка опция с заголовком '{testTitle.Title}'");
+            return new WebItem($"//div[@class='option']//a[contains(text(), '{optionDetail.Title}')]",
+                $"Ссылка опция с заголовком '{optionDetail.Title}'");
         }
         public UserStoryMapPage RefreshPage()
         {
             WebDriverActions.Refresh();
             return this;
         }
-        public SliderOptionViewPage OpenOptionView(Bitrix24USMOption testTitle)
+        public SliderOptionViewPage OpenOptionView(Bitrix24USMOption optionDetail)
         {
-            WebItem createdOptionByTest = GetOptionLinkByTitle(testTitle);
+            WebItem createdOptionByTest = GetOptionLinkByTitle(optionDetail);
             createdOptionByTest.Click();
             return new SliderOptionViewPage();
         }
-        public UserStoryMapPage CreateOption(Bitrix24USMOption testTitle)
+        public UserStoryMapPage CreateOption(Bitrix24USMOption optionDetail)
         {
             btnAddOption.Click();
-            inputNewOption.SendKeys(testTitle.Title + Keys.Enter);
+            inputNewOption.SendKeys(optionDetail.Title + Keys.Enter);
             RefreshPage();
             return this;
         }
-        public bool IsOptionExist(Bitrix24USMOption testTitle)
+        public bool IsOptionExist(Bitrix24USMOption optionDetail)
         {
             WebItem ActorOnUSMPage = new WebItem("//div[contains(@class, 'card-column-header actor')]", "Актер в USM");
             ActorOnUSMPage.WaitElementDisplayed();
-            WebItem createdOptionByTest = GetOptionLinkByTitle(testTitle);
+            WebItem createdOptionByTest = GetOptionLinkByTitle(optionDetail);
             return createdOptionByTest.WaitElementDisplayed();
         }
     }
